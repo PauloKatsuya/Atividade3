5f7e8ef0640f7493d88b3332cd31790b741b804a
PauloKatsuya <katsuyapaulo@gmail.com>
2024-09-06 20:53:31 -0300

    Create escultura.java

---
+++ escultura.java
@@ -0,0 +1,22 @@
+public class Escultura {
+	public double pose;
+	public String material;
+	public String equipamento;
+	
+	public Escultura(double angulo, String pedra, String cincel) {
+		this.pose = angulo;
+		this.material = pedra;
+		this.equipamento = cincel;
+	}
+ 
+	public void decorar() {
+		System.out.println("Decorar o ambiente");
+	}
+	public void representarHistoria() {
+		System.out.println("Historia");
+	}
+	public void esculpir() {
+		System.out.println("Escultura esculpida!");
+	}
+	
+}
