28982d52ebeadfcf235ad14645caf3d31771a642
PauloKatsuya <katsuyapaulo@gmail.com>
2024-09-06 20:49:08 -0300

    Create elefante.java

---
+++ elefante.java
@@ -0,0 +1,19 @@
+public class Elefante {
+	public double tromba;
+	public double peso;
+	public double tamanho;
+	
+	public Elefante(double tamanhoTromba, double quilos, double metros) {
+		this.tromba = tamanhoTromba;
+		this.peso = quilos;
+		this.tamanho = metros;
+	}
+ 
+	public void agarrarTromba() {
+		System.out.println("Pegando com a Tromba");
+	}
+	public void usarMarfim() {
+		System.out.println("Usando Marfim");
+	}
+	
+}
