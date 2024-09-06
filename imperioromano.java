ec2bfb4c4d08c4763347fef582dd2debf58bd07b
PauloKatsuya <katsuyapaulo@gmail.com>
2024-09-06 20:51:45 -0300

    Create imperioromano.java

---
+++ imperioromano.java
@@ -0,0 +1,16 @@
+public class ImperioRomano{
+    public String historico;
+    public String exercito;
+    public String local;
+    
+    public Carro(string historicoVar, string exercitoVar, string localVar){
+        this.historico = historicoVar; 
+        this.exercito = exercitoVar;
+        this.local = localVar;
+    }
+    
+    public void definirTerritorio(){
+        System.out.println("Definindo local");
+        System.out.println("Local definido");
+    }
+}
