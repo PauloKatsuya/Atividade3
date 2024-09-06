5854682590d0c2c2cdbe04a4edda4bdf4687c682
PauloKatsuya <katsuyapaulo@gmail.com>
2024-09-06 20:54:17 -0300

    Create leao.java

---
+++ leao.java
@@ -0,0 +1,22 @@
+public class leao {
+	public Double juba;
+	public double pelo;
+	public String rugido;
+	
+	public leao(Double comprimentoJuba, Double comprimentoPelo, String tomDoRugido) {
+		this.juba = comprimentoJuba;
+		this.pelo = comprimentoPelo;
+		this.rugido = tomDoRugido;
+	}
+ 
+	public void dominar() {
+		System.out.println("Territorio Dominado!");
+	}
+	public void batalhar() {
+		System.out.println("Batalha vencida");
+	}
+	public void liderar() {
+		System.out.println("Bando liderado");
+	}
+}
+ 
