1325292c6417d238a6936c3d272741c8bd1e33c1
PauloKatsuya <katsuyapaulo@gmail.com>
2024-09-06 20:45:11 -0300

    Create Smartphone.java

---
+++ Smartphone.java
@@ -0,0 +1,22 @@
+public class Smartphone {
+	public String modelo;
+	public double tela;
+	public double camera;
+	
+	public Smartphone(String marca, double polegadas, double resolucao) {
+		this.modelo = marca;
+		this.tela = polegadas;
+		this.camera = resolucao;
+	}
+ 
+	public void tirarFoto() {
+		System.out.println("Foto tirada!");
+	}
+	public void acessarInternet() {
+		System.out.println("Conectando...");
+		System.out.println("Conectado.");
+	}
+	public void manipularFoto() {
+		System.out.println("Imagem editada");
+	}
+}
